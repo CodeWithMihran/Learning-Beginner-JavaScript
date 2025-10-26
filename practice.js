5cdaea6cc76adad0f43f418e3536b9c7a181d375
CodeWithMihran <sohail.mihran@gmail.com>
2025-10-26 20:47:46 +0530

    Learning JavaScript (Data Types)

--- practice.js
+++ practice.js
@@ -13,9 +13,28 @@
 // console.log(isFollow);
 
 
-let name = "Tony Stark";
+// let name = "Tony Stark";
+// let age = 24;
+
+// const PI = 3.14;
+
+// console.log(PI);
+
+
+// Data Types
+
 let age = 24;
+let name = "Tony Stark";
+let isFollow = true;
+let x = null;
+let y;
+let z = BigInt("12345");
+let a = Symbol("Hello!");
 
-const PI = 3.14;
-
-console.log(PI);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isFollow);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof a);
