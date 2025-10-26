319ec0830483524edca25cd0433b74b3c49c4121
CodeWithMihran <sohail.mihran@gmail.com>
2025-10-26 21:00:32 +0530

    Learning JavaScript (Non-Primitive Data Type[Object])

--- practice.js
+++ practice.js
@@ -23,18 +23,34 @@
 
 // Data Types
 
-let age = 24;
-let name = "Tony Stark";
-let isFollow = true;
-let x = null;
-let y;
-let z = BigInt("12345");
-let a = Symbol("Hello!");
+// let age = 24;
+// let name = "Tony Stark";
+// let isFollow = true;
+// let x = null;
+// let y;
+// let z = BigInt("12345");
+// let a = Symbol("Hello!");
 
-console.log(typeof age);
-console.log(typeof name);
-console.log(typeof isFollow);
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
-console.log(typeof a);
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isFollow);
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+// console.log(typeof a);
+
+
+// Non-Primitive Data Types
+
+const student = {
+    name : "Md Mihran Sohail",
+    age : 17,
+    cgpa : 8.7,
+    isPass : true,
+};
+
+console.log(student);
+console.log(student["age"]);
+
+student["age"] = student["age"] + 1;
+console.log(student.age);
