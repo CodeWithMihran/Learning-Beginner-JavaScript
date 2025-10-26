9c84b874015fb724109afa91ccec90c77b1e0d9d
CodeWithMihran <sohail.mihran@gmail.com>
2025-10-26 20:26:45 +0530

    Learning JavaScript (Variables)

--- practice.js
+++ practice.js
@@ -1,3 +1,13 @@
-console.log("Hello World !!");
-console.log("Md Mihran Sohail");
+// console.log("Hello World !!");
+// console.log("Md Mihran Sohail");
 
+
+name = "Tony Stark"
+age = 24;
+
+x = null;
+y = undefined;
+ 
+isFollow = true;
+
+console.log(isFollow);
