35143e85c7856b410db1d391156dc9ae00f58628
CodeWithMihran <sohail.mihran@gmail.com>
2025-10-27 11:13:12 +0530

    Learning JavaScript (Arithmetic Operations)

--- script.js
+++ script.js
@@ -56,28 +56,50 @@
 // console.log(student.age);
 
 
-// Practice Question 1
+// // Practice Question 1
 
-const product = {
-    title : "Parker Jotter Standard CT Ball Pen (Black)",
-    rating : 4,
-    isDeal : true,
-    offer : 5,
-    price : 270,
-};
+// const product = {
+//     title : "Parker Jotter Standard CT Ball Pen (Black)",
+//     rating : 4,
+//     isDeal : true,
+//     offer : 5,
+//     price : 270,
+// };
 
-console.log(product);
+// console.log(product);
 
-// Pactice Question 2
+// // Pactice Question 2
 
-const profile = {
-    username : "CodeWithMihran",
-    isFollow : false,
-    followers : 78,
-    following : 23,
-    posts : 24,
-};
+// const profile = {
+//     username : "CodeWithMihran",
+//     isFollow : false,
+//     followers : 78,
+//     following : 23,
+//     posts : 24,
+// };
 
-console.log(profile);
-console.log(typeof profile);
-console.log(typeof profile["isFollow"]);
+// console.log(profile);
+// console.log(typeof profile);
+// console.log(typeof profile["isFollow"]);
+
+
+// Arithmetic Operations
+
+let x = 5;
+let y = 2;
+
+console.log("x = ",x," & y = ",y);
+console.log("x + y = ", x+y);
+console.log("x - y = ", x-y);
+console.log("x * y = ", x*y);
+console.log("x / y = ", x/y);
+console.log("x % y = ", x%y);
+console.log("x ** y = ", x**y);
+
+// Urinary Operators
+
+x++; 
+console.log("x = ",x);
+
+y--;
+console.log("y = ",y);
