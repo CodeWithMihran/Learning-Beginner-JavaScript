bd06a2ba10a48744f5a17bff4b2087b5af84ce46
CodeWithMihran <sohail.mihran@gmail.com>
2025-10-28 21:43:32 +0530

    Learning JavaScript (Practice Question 11)

--- script.js
+++ script.js
@@ -333,13 +333,29 @@
 
 // Arrow Function 
 
-const arrowSum = (a, b) => {
-    console.log(a+b);
+// const arrowSum = (a, b) => {
+//     console.log(a+b);
+// }
+
+// const arrowMult = (a, b) => {
+//     console.log(a * b);
+// }
+
+// arrowSum(3,4);
+// arrowMult(4,5);
+
+// Practice Question 11
+
+function numOfVowels(str){
+    let count = 0;
+    for(let i=0; i<str.length; i++){
+        if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u" 
+           || str[i] === "A" || str[i] === "E" || str[i] === "I" || str[i] === "O" || str[i] === "U"){
+            count ++;
+        }
+    }
+    return count;
 }
 
-const arrowMult = (a, b) => {
-    console.log(a * b);
-}
-
-arrowSum(3,4);
-arrowMult(4,5);
+let str = prompt("Enter a word (string) : ");
+console.log("The number of vowels in the given string are : ", numOfVowels(str));
